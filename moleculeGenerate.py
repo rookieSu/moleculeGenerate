bd531970b8e192893dbbe805a611f5754c3d18e8
su <hurrysu@163.com>
2018-12-06 22:20:29 +0800

    add ben.mol and test ben.mol

--- moleculeGenerate.py
+++ moleculeGenerate.py
@@ -16,54 +16,50 @@ def read_file(filename):
            x, y, z = eval(lines[i+4].split()[0]), eval(lines[i+4].split()[1]), eval(lines[i+4].split()[2])
            atomDict[i+1] = lines[i+4].split()[3]
            coordinates.append([x,y,z])
-           temp = []
-           for j in range(0, atomNumber):
-               temp.append(0)
-           bondAdj.append(temp)
+           bondAdj.append([0 for x in range(0, atomNumber)])
         for i in range(atomNumber+4,len(lines)-1):
-            row, column = eval(lines[i].split()[0]), eval(lines[i].split()[1])
+            row, column = eval(lines[i].split()[0])-1, eval(lines[i].split()[1])-1
             if eval(lines[i].split()[2]) == 2:
-                bondAdj[row-1][column-1] = 2 
-                bondAdj[column-1][row-1] = 2
+                bondAdj[row][column] = 2 
+                bondAdj[column][row] = 2
             else: 
-                bondAdj[row-1][column-1] = 1
-                bondAdj[column-1][row-1] = 1  
+                bondAdj[row][column] = 1
+                bondAdj[column][row] = 1  
     f.close()
 
 def is_replace():
     global replace, bondAdj
     bondCounter = [sum(bond) for bond in bondAdj]
-    print(bondCounter)
-    for (count, index) in zip(bondCounter,range(0, atomNumber)):
+    for (count, index) in zip(bondCounter,range(1, atomNumber+1)):
         if count == 5:
             pass
         elif count == 4:
-            if atomDict[index+1] == 'N':
-                replace[index+1] = 1
-            else:replace[index+1] = 0
+            if atomDict[index] == 'N':
+                replace[index] = 1
+            else:replace[index] = 0
         elif count == 2:
-            if atomDict[index+1] == 'N':
-                replace[index+1] = 1
-            elif atomDict[index+1] == 'C':
-                replace[index+1] = 2
-            else:replace[index+1] = 0
+            if atomDict[index] == 'N':
+                replace[index] = 1
+            elif atomDict[index] == 'C':
+                replace[index] = 2
+            else:replace[index] = 0
         elif count == 3:
-            if atomDict[index+1] == 'C':
-                replace[index+1] = 1
-            else:replace[index+1] = 0
+            if atomDict[index] == 'C':
+                replace[index] = 1
+            else:replace[index] = 0
         elif count == 1:
-            if atomDict[index+1] == 'O':
-                replace[index+1] = 1
-            elif atomDict[index+1] == 'N':
-                replace[index+1] = 2
-            elif atomDict[index+1] == 'C':
-                replace[index+1] = 3
-            else:replace[index+1] = 0
+            if atomDict[index] == 'O':
+                replace[index] = 1
+            elif atomDict[index] == 'N':
+                replace[index] = 2
+            elif atomDict[index] == 'C':
+                replace[index] = 3
+            else:replace[index] = 0
         else:
-            replace[index+1] = 0
+            replace[index] = 0
                 
 if __name__ == "__main__":
-    read_file('p35_173.mol')
+    read_file('ben.mol')
     is_replace()
     print(replace)
 
